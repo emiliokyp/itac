d78f124cbc356226fd07ccbfcf93874bcbfccd05
Emilio <emilio.kyp@gmail.com>
2018-05-01 10:18:11 +1000

    back

--- views/pricing.view.php
+++ views/pricing.view.php
@@ -3,4 +3,5 @@
 require './components/partials/header.php';
 require './components/sub-hero.php';
 require './components/pricing-table-adv.php';
+require './components/fixed-prices.php';
 require './components/partials/footer.php';
