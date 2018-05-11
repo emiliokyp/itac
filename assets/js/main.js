f7aa6e9c96eae3d6446eec001094610898e693a9
Emilio <emilio.kyp@gmail.com>
2018-05-11 15:22:46 +1000

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,135 +1,137 @@
 // nav shrinking and animating
 
-function nav() {
+$(document).ready(function() {
+  function nav() {
     if ($(document).scrollTop() > 100 || window.location.pathname != '/') {
-        $('#nav').addClass('shrink');
-        $('#burger').addClass('black');
+      $('#nav').addClass('shrink');
+      $('#burger').addClass('black');
     } else {
-        $('#nav').removeClass('shrink');
-        $('#burger').removeClass('black');
+      $('#nav').removeClass('shrink');
+      $('#burger').removeClass('black');
     }
-}
+  }
 
-nav();
-$(document).on('scroll', nav);
+  nav();
+  $(document).on('scroll', nav);
 
-$('#burger').on('click', function() {
+  $('#burger').on('click', function() {
     $('.drop-down').toggleClass('menu-active');
     $('#burger').toggleClass('close');
-});
+  });
 
-$('.drop-down ul').on('click', function() {
+  $('.drop-down ul').on('click', function() {
     $('.drop-down').removeClass('menu-active');
     $('#burger').removeClass('close');
-});
+  });
 
-// Nav jerry rigging
+  // Nav jerry rigging
 
-document.querySelector('#logo').addEventListener('click', () => {
+  document.querySelector('#logo').addEventListener('click', () => {
     window.location.pathname = '/';
-});
+  });
 
-$('.nav-list').on('click', function(e) {
+  $('.nav-list').on('click', function(e) {
     if (window.location.pathname === '/') {
-        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-            anchor.addEventListener('click', function(e) {
-                e.preventDefault();
-                document.querySelector(this.getAttribute('href')).scrollIntoView({
-                    behavior: 'smooth',
-                    inline: 'start',
-                    block: 'start'
-                });
-            });
+      document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+          e.preventDefault();
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            inline: 'start',
+            block: 'start'
+          });
         });
+      });
     } else if (e.target.innerHTML === 'HOME') {
-        window.location.pathname = '/';
-        window.location.pathname = '/';
+      window.location.pathname = '/';
+      window.location.pathname = '/';
     } else if (e.target.innerHTML === 'SOLUTIONS') {
-        window.location.pathname = '/industry-solutions';
-        window.location.hash = '';
+      window.location.pathname = '/industry-solutions';
+      window.location.hash = '';
     } else if (e.target.innerHTML === 'SERVICES') {
-        window.location = '/#services';
-        window.location.hash = '';
+      window.location = '/#services';
+      window.location.hash = '';
     } else if (e.target.innerHTML === 'PRICING') {
-        window.location.pathname = '/pricing';
-        window.location.pathname = '/pricing';
+      window.location.pathname = '/pricing';
+      window.location.pathname = '/pricing';
     } else if (e.target.innerHTML.trim() === 'CONTACT') {
-        window.location.pathname = '/contact';
-        window.location.pathname = '/contact';
+      window.location.pathname = '/contact';
+      window.location.pathname = '/contact';
     }
-});
+  });
 
-// type writer effect on hero
+  // type writer effect on hero
 
-document.addEventListener('DOMContentLoaded', function(event) {
+  document.addEventListener('DOMContentLoaded', function(event) {
     var dataText = ['Problem?', 'Solved. :)', 'Top Notch Tech Support'];
 
     function typeWriter(text, i, fnCallback) {
-        if (i < text.length) {
-            document.querySelector('.hero h1').innerHTML =
-                text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
+      if (i < text.length) {
+        document.querySelector('.hero h1').innerHTML =
+          text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
 
-            setTimeout(function() {
-                typeWriter(text, i + 1, fnCallback);
-            }, 100);
-        } else if (typeof fnCallback == 'function') {
-            setTimeout(fnCallback, 700);
-        }
+        setTimeout(function() {
+          typeWriter(text, i + 1, fnCallback);
+        }, 100);
+      } else if (typeof fnCallback == 'function') {
+        setTimeout(fnCallback, 700);
+      }
     }
 
     function StartTextAnimation(i) {
-        if (typeof dataText[i] == 'undefined') {
-            setTimeout(function() {
-                StartTextAnimation(0);
-            }, 20000);
-            return;
-        }
-        if (i < dataText[i].length) {
-            typeWriter(dataText[i], 0, function() {
-                StartTextAnimation(i + 1);
-            });
-        }
+      if (typeof dataText[i] == 'undefined') {
+        setTimeout(function() {
+          StartTextAnimation(0);
+        }, 20000);
+        return;
+      }
+      if (i < dataText[i].length) {
+        typeWriter(dataText[i], 0, function() {
+          StartTextAnimation(i + 1);
+        });
+      }
     }
     StartTextAnimation(0);
-});
+  });
 
-// brands we love owl slider
+  // brands we love owl slider
 
-$(document).ready(function() {
+  $(document).ready(function() {
     $('.owl-carousel').owlCarousel({
-        items: 4,
-        loop: true,
-        margin: 10,
-        autoplay: true,
-        autoplayTimeout: 2000,
-        autoplayHoverPause: false,
-        responsiveClass: true,
-        center: true,
-        autowidth: true,
-        responsive: {
-            0: {
-                items: 2,
-                nav: false
-            },
-            600: {
-                items: 2,
-                nav: false
-            },
-            1000: {
-                items: 5,
-                nav: false,
-                loop: true
-            }
+      items: 4,
+      loop: true,
+      margin: 10,
+      autoplay: true,
+      autoplayTimeout: 2000,
+      autoplayHoverPause: false,
+      responsiveClass: true,
+      center: true,
+      autowidth: true,
+      responsive: {
+        0: {
+          items: 2,
+          nav: false
+        },
+        600: {
+          items: 2,
+          nav: false
+        },
+        1000: {
+          items: 5,
+          nav: false,
+          loop: true
         }
+      }
     });
-});
+  });
 
-// google recaptcha validation check
+  // google recaptcha validation check
 
-var form = document.querySelector('#contact-form');
-form.addEventListener('submit', function(e) {
+  var form = document.querySelector('#contact-form');
+  form.addEventListener('submit', function(e) {
     if (grecaptcha.getResponse() === '') {
-        e.preventDefault();
-        alert('Please check the recaptcha');
+      e.preventDefault();
+      alert('Please check the recaptcha');
     }
-});
+  });
+}); // end document. ready;
