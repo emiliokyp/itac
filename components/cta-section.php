bd9207f2cfe56b6613bf7388a510d11e2718ab6e
Emilio <emilio.kyp@gmail.com>
2018-06-26 11:30:09 +1000

    fixed cta section

--- components/cta-section.php
+++ components/cta-section.php
@@ -1,8 +1,8 @@
 <section id="cta-section">
 <div class="cta-container">
-    <h2 class="cta-title">We’re here to save you time, stress, heartache and money with expert guidance in all things technology</h2>
-<h3 class="cta-subtitle">Find out how we can help you</h3>
-<button  onclick="Calendly.showPopupWidget('https://calendly.com/itactechnology');return false;"class="learn-more-pricing" style="width: 400px; height: 60px;">Book your <strong>free</strong> intro session today</button>
+    <h2 class="cta-title">We’re here to save you time, stress, heartache and money with expert guidance in all things technology, find out how we can help you.</h2>
+<h3 class="cta-subtitle"></h3>
+<button  onclick="Calendly.showPopupWidget('https://calendly.com/itactechnology');return false;"class="learn-more-pricing cta-button">Book your free intro session today</button>
 <i><a href="#testimonials">Still not convinced?</a></i>
 </div>
 </section>
